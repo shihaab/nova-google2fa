8683004885303e6a3ba5a78ca48bd0554f0e1ffd
Shihaab <roushi02@gmail.com>
2021-03-10 12:33:24 +0100

    Debug

--- resources/views/recovery.blade.php
+++ resources/views/recovery.blade.php
@@ -11,14 +11,14 @@
     <link rel="stylesheet" href="{{ mix('app.css', 'vendor/nova') }}">
 
     <style>
-        body {
-            font-family: "Montserrat", sans-serif !important;
+        .text-primary {
+            color: #039f3c!important;
         }
-
-        .btn,
-        .form-input,
-        .rounded-lg {
-            border-radius: 0 !important;
+        .btn-primary {
+            background-color: #039f3c!important;
+        }
+        button::selection {
+            outline: none!important;
         }
         @media print
         {
@@ -32,25 +32,27 @@
 <body class="bg-40 text-black h-full">
 <div class="h-full">
     <div class="px-view py-view mx-auto">
-        <div class="mx-auto py-8 max-w-sm text-center text-90">
+        <div class="mx-auto py-8 max-w-sm text-center font-bold text-90">
             @include('nova::partials.logo')
         </div>
 
         <form class="bg-white shadow rounded-lg p-8 max-w-xl mx-auto" method="POST" action="/los/2fa/register">
-            <h2 class="p-2">Recovery codes</h2>
+            <h2 class="p-2">Tweefactorauthenticatie Mijn Digitale Buddy</h2>
             @csrf
             <p class="p-2">
-                Recovery codes are used to access your account in the event you cannot recive two-factor
-                authentication codes.
+                Omdat jouw account nog geen connectie met Google Authenticator heeft zijn deze nu aangemaakt. 
+            </p>
+            <p class="p-2">
+                Herstelcodes worden gebruikt om toegang te krijgen tot uw account in het geval u geen tweefactorauthenticatiecodes kunt ontvangen.
             </p>
             <p class="p-2 no-print">
-                <strong>
-                    Download, print or copy your codes before continuing two-factor authentication setup.
+                <strong style="color: #3182ce;">
+                Download, print of kopieer uw code voordat u doorgaat met het instellen van tweestapsverificatie.
                 </strong>
             </p>
             <div class="p-3">
-                <label class="block font-bold mb-2" for="co">Recovery codes
-                    <button class="no-print m-1  btn btn-default btn-primary hover:bg-primary-dark" type="button"
+                <label class="block font-bold mb-2" for="co">Herstel codes:
+                    <button style="font-weight: 900;" class="no-print text-primary dim font-bold no-underline" type="button"
                             onclick="window.print();return false;">
                         Print
                     </button>
@@ -59,14 +61,14 @@
                 <div>
                     @foreach ($recovery as $recoveryCode)
                         <ul>
-                            <li class="p-2">{{ $recoveryCode }}</li>
+                            <li style="font-family: monospace;" class="p-2">{{ $recoveryCode }}</li>
                         </ul>
                     @endforeach
                 </div>
             </div>
 
-            <button class="no-print m-2 w-1/2 btn btn-default btn-primary hover:bg-primary-dark" type="submit">
-                Continue
+            <button class="no-print text-primary dim font-bold text-right w-full" type="submit">
+                Ga verder
             </button>
         </form>
     </div>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,16 +9,12 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('app.css', 'vendor/nova') }}">
-
     <style>
-        body {
-            font-family: "Montserrat", sans-serif !important;
+        .text-primary {
+            color: #039f3c!important;
         }
-
-        .btn,
-        .form-input,
-        .rounded-lg {
-            border-radius: 0 !important;
+        .btn-primary {
+            background-color: #039f3c!important;
         }
     </style>
     <script>
@@ -32,43 +28,37 @@
 <body class="bg-40 text-black h-full">
 <div class="h-full">
     <div class="px-view py-view mx-auto">
-        <div class="mx-auto py-8 max-w-sm text-center text-90">
+        <div class="mx-auto py-8 max-w-sm text-center font-bold text-90">
             @include('nova::partials.logo')
         </div>
 
-        <form id="register_form" class="bg-white shadow rounded-lg p-8 max-w-xl mx-auto" method="POST"
+        <form id="register_form" class="bg-white shadow rounded-lg p-8 max-w-login mx-auto" method="POST"
               action="/los/2fa/confirm">
             @csrf
-            <h2 class="p-2">Two Factor Authentication</h2>
-
-            <p class="p-2">Two factor authentication (2FA) strengthens access security by requiring two methods (also
-                referred
-                to as factors) to verify your identity. Two factor authentication protects against phishing, social
-                engineering and password brute force attacks and secures your logins from attackers exploiting weak
-                or stolen credentials.</p>
-            <p class="p-2">To Enable Two Factor Authentication on your Account, you need to do following steps</p>
-            <strong>
-                <ol>
-                    <li>Verify the OTP from Google Authenticator Mobile App</li>
-                </ol>
-            </strong>
+            <h2 class="text-2xl text-center font-normal mb-6 text-90">Tweefactorauthenticatie</h2>
+            <svg class="block mx-auto mb-6" xmlns="http://www.w3.org/2000/svg" width="100" height="2" viewBox="0 0 100 2">
+                <path fill="#D8E3EC" d="M0 0h100v2H0z"></path>
+            </svg>
+            
+            <p class="p-2">Om tweefactorauthenticatie in te schakelen voor uw account, moet u de code van de mobiele Google Authenticator-app invullen</p>
+            <p class="p-2">Scan de QR-code via de app. <a target="_blank" href="https://support.google.com/accounts/answer/1066447?co=GENIE.Platform%3DiOS&hl=nl" class="no-print text-primary dim font-bold no-underline">Help</a></p>
             <div class="text-center">
                 <img src="{{ $google2fa_url }}" alt="">
             </div>
 
             <div class="text-center">
-                <div class="mb-6 w-1/2" style="display:inline-block">
+                <div class="mb-6" style="display:inline-block">
                     @if (isset($error))
                         <p class="text-center font-semibold text-danger my-3">
                             {{  $error }}
                         </p>
                     @endif
-                    <label class="block font-bold mb-2" for="co">Secret</label>
+                    <label class="block font-bold mb-2" for="co">Authenticatie code</label>
                     <input class="form-control form-input form-input-bordered w-full" id="secret" type="number"
                            name="secret" value="" required="required" onkeyup="checkAutoSubmit(this)" autofocus="">
                 </div>
-                <button class="w-1/2 btn btn-default btn-primary hover:bg-primary-dark" type="submit">
-                    Confirm
+                <button class="w-full btn btn-default btn-primary hover:bg-primary-dark" type="submit">
+                    Bevestig
                 </button>
             </div>
         </form>
